5b951b4e050545a40c9a7aafa06c7441c62d4518
millaguix-dev <carlosmillan642@gmail.com>
2022-05-31 12:15:19 -0400

    Create PrimeraParte.java

---
+++ PrimeraParte.java
@@ -0,0 +1,14 @@
+public class PrimeraParte {
+    public static void main(String[] args) throws Exception {
+        int resultado;
+        resultado=suma(10,25,5);
+        System.out.println(resultado);
+    }
+
+    static int suma(int a, int b, int c)
+{
+    return a+b+c;
+
+}
+}
+
